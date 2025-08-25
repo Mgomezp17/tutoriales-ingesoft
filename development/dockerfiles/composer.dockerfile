19038030f84f54e3ee539baa41eaac9b85a82d3b
mgomezp17 <mgomezp17@eafit.edu.co>
2025-08-25 15:36:29 -0500

    first commit

---
+++ development/dockerfiles/composer.dockerfile
@@ -0,0 +1,12 @@
+FROM composer:latest
+
+ARG UID=501
+ARG GID=20
+
+RUN addgroup -g 1000 laravel && adduser -G laravel -g laravel -s /bin/sh -D laravel
+
+USER laravel
+
+WORKDIR /var/www/html
+
+ENTRYPOINT [ "composer", "--ignore-platform-reqs" ]
---
+++ laravelcourse/app/Http/Controllers/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View|RedirectResponse
+    {
+        if (! is_numeric($id)) {
+            return redirect()->route('product.index');
+        }
+
+        $viewData = [];
+        $product = Product::findOrFail($id);
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
+    }
+
+    public function create(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Create product';
+
+        return view('product.create')->with('viewData', $viewData);
+    }
+
+    public function save(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|gt:0',
+        ]);
+        Product::create($request->only(['name', 'price']));
+
+        return back();
+    }
+}
