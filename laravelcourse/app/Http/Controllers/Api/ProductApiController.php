9ae26a5359dd0969dc5aba2f6e54c9940940bce6
mgomezp17 <mgomezp17@eafit.edu.co>
2025-09-29 21:12:39 -0500

    Remove unused import of RedirectResponse in ProductApiController

--- laravelcourse/app/Http/Controllers/Api/ProductApiController.php
+++ laravelcourse/app/Http/Controllers/Api/ProductApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProductApiController extends Controller
