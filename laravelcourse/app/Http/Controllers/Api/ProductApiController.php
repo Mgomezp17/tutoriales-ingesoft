633736aa06a73fc2334c80087fc2f0156b033565
mgomezp17 <mgomezp17@eafit.edu.co>
2025-09-29 19:01:39 -0500

    Add API controllers and resources for product management
    - Created ProductApiController for basic product operations.
    - Introduced ProductApiControllerV2 and ProductApiControllerV3 for enhanced product resource handling and pagination.
    - Added ProductResource and ProductCollection for structured API responses.
    - Defined API routes for product endpoints in api.php.
    - Included a test HTML file for API interaction.

---
+++ laravelcourse/app/Http/Controllers/Api/ProductApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class ProductApiController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = Product::all();
+        return response()->json($products, 200);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product, 200);
+    }
+
+    // Activity 2
+    public function save(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|gt:0',
+        ]);
+
+        $product = new Product();
+        $product->setName($request->name);
+        $product->setPrice($request->price);
+        $product->save();
+
+        return response()->json($product, 200);
+    }
+}
